9d260355cffcca7998df0b2504de7b263a57284f
Z1pakk <wladik420@gmail.com>
2019-02-03 19:51:15 +0200

    Select from table

--- AdoNetConnection/Program.cs
+++ AdoNetConnection/Program.cs
@@ -12,31 +12,46 @@ namespace AdoNetConnection
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
-            Console.InputEncoding= System.Text.Encoding.Unicode;
-            string firstName=string.Empty;
-            string lastName = string.Empty;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            //string firstName = string.Empty;
+            //string lastName = string.Empty;
 
-            Console.WriteLine("Реєстрація автора");
-            Console.Write("Введіть ім'я автора=>>");
-            firstName=Console.ReadLine();
-            Console.Write("Введіть прізвище автора=>>");
-            lastName = Console.ReadLine();
-
+            //Console.WriteLine("Реєстрація автора");
+            //Console.Write("Введіть ім'я автора=>>");
+            //firstName = Console.ReadLine();
+            //Console.Write("Введіть прізвище автора=>>");
+            //lastName = Console.ReadLine();
             //Клас самого підлючення
             SqlConnection connection = new SqlConnection();
-            //Рядок підключення до БД
-            connection.ConnectionString = 
-            "Data Source=.;Initial Catalog=AdoNetLibrary;Integrated Security=True";
-            //Відкриття підлкючення дл БД
-            connection.Open();
-            //Рядок запиту
-            string insertString = $@"INSERT INTO dbo.tbl_Authors VALUES ('{firstName}','{lastName}')";
-            //Із рядка комнади зроибли саму команду
-            SqlCommand command = new SqlCommand(insertString, connection);
-            //Виконать команду
-            Console.WriteLine($"Успішно додано {command.ExecuteNonQuery()} авторів");
-            //Закриття підключення до БД
-            connection.Close();
+            SqlDataReader reader = null;
+            try
+            {
+                //Рядок підключення до БД
+                connection.ConnectionString =
+                "Data Source=.;Initial Catalog=AdoNetLibrary;Integrated Security=True";
+                //Відкриття підлкючення дл БД
+                connection.Open();
+                SqlCommand command = new SqlCommand("Select * from dbo.tbl_Authors",connection);
+                reader = command.ExecuteReader();
+                Console.WriteLine("Список авторів:");
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[1]+" "+reader[2]);
+                }
+                //Рядок запиту
+                //string insertString =
+                //    $@"INSERT INTO dbo.tbl_Authors VALUES ('{firstName}','{lastName}')";
+                ////Із рядка комнади зроибли саму команду
+                //SqlCommand command = new SqlCommand(insertString, connection);
+                //Виконать команду
+                //Console.WriteLine($"Успішно додано {command.ExecuteNonQuery()} авторів");
+                //Закриття підключення до БД
+            }
+            finally
+            {
+                reader.Close();
+                connection.Close();
+            }
         }
     }
 }
