aa684f1c4bc07e4a7763f9a7402b259f22daf081
Z1pakk <wladik420@gmail.com>
2019-02-03 17:53:50 +0200

    First commit

---
+++ AdoNetConnection/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Клас самого підлючення
+            SqlConnection connection = new SqlConnection();
+            //Рядок підключення до БД
+            connection.ConnectionString = 
+            "Data Source=.;Initial Catalog=AdoNetLibrary;Integrated Security=True";
+            //Відкриття підлкючення дл БД
+            connection.Open();
+            //Рядок запиту
+            string insertString = @"INSERT INTO dbo.tbl_Authors VALUES ('Georgiy','Kesha')";
+            //Із рядка комнади зроибли саму команду
+            SqlCommand command = new SqlCommand(insertString, connection);
+            //Виконать команду
+            command.ExecuteNonQuery();
+            //Закриття підключення до БД
+            connection.Close();
+        }
+    }
+}
